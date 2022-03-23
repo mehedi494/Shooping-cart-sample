cd786bc91b0033a4ebb0c39c0bd13b892fc9a5ac
mehedi494 <ehasanmunna21@gmail.com>
2022-03-23 17:08:19 +0600

    check out section added

--- app.js
+++ app.js
@@ -1,46 +1,74 @@
 // Quantity Increse Deecres Function
-function quantity(documentId, priceId,price,isAdding) {
+function quantity(documentId, priceId, price, isAdding) {
     const inputValueTxt = document.getElementById(documentId)
     const inputValueString = inputValueTxt.value
     const inputValueNumber = parseInt(inputValueString)
 
     const priceField = document.getElementById(priceId)
-    // const priceTxt = priceField.innerText
-   
 
-    if (isAdding  ==true && inputValueNumber >=0) {
-        const newPlusInputValue = inputValueNumber + 1;
-        return newPlusInputValue;
-        // inputValueTxt.value = newPlusInputValue;
-        // const newPrice = price * newPlusInputValue
-        // priceField.innerText = newPrice  ;
+    let newInputValue = 0;
+
+    if (isAdding == true && inputValueNumber >= 0) {
+        newInputValue = inputValueNumber + 1;
+
     }
-    else if (isAdding== false && inputValueNumber >0) {
-        const newMinusInputValue = inputValueNumber - 1;
-        return newMinusInputValue;
-        // inputValueTxt.value = newMinusInputValue;
-        // const dcresNewPrice = price * newMinusInputValue
-        // priceField.innerText = dcresNewPrice;
+    else if (isAdding == false && inputValueNumber > 0) {
+        newInputValue = inputValueNumber - 1;
+
     }
-        inputValueTxt.value = newPlusInputValue;
-        const newPrice = price * newMinusInputValue
-        priceField.innerText = newPrice  ;
+    inputValueTxt.value = newInputValue;
+    const newPrice = price * newInputValue
+    priceField.innerText = newPrice;
+
+       subTotal()
+
 }
 
-// update
+function subTotal() {
+
+    // SubTotal Section
+    const subTotalField = document.getElementById("subTotalId");
+    const mobilePriceString = document.getElementById("mobilePrice")
+    const casePriceString = document.getElementById("casePrice")
+    const casePrice = parseFloat(casePriceString.innerText)
+    const mobilePrice = parseFloat(mobilePriceString.innerText)
+    const subtotal = casePrice + mobilePrice;
+    subTotalField.innerText = subtotal;
+
+
+// Tax Section
+    const taxAmountTxt = document.getElementById("taxId")
+    
+    const taxAmount = Math.ceil((subtotal / 100) * 5);
+    taxAmountTxt.innerText = taxAmount;
+
+    // Total Amount
+    const totalField = document.getElementById("totalId")
+    totalField.innerText = taxAmount + subtotal;
+
+}
+
+document.getElementById("checkOutId").addEventListener("click",function () {
+    // const modal = document.getElementById("modal")
+    document.getElementById("paymentDiv").style.display ="block";
+    console.log("ok")
+   
+})
+
+// update Quantity
 
 document.getElementById("mobile-plus-btn").addEventListener("click", function () {
-    // const inputValueTxt = document.getElementById("mobile-input-number")
-    // const inputValueString = inputValueTxt.value
-    // const inputValueNumber = parseInt(inputValueString)
-    // const newInputValue = inputValueNumber + 1;
-    // inputValueTxt.value = newInputValue;
-   
-    const mobileIncrese = quantity("mobile-input-number", "mobilePrice",1219,true)
+    quantity("mobile-input-number", "mobilePrice", 1219, true)
 })
 
 document.getElementById("mobile-minus-btn").addEventListener("click", function () {
-    
-    const mobileIncrese = quantity("mobile-input-number", "mobilePrice",1219, false)
+    quantity("mobile-input-number", "mobilePrice", 1219, false)
 
+})
+document.getElementById("case-plus-btn").addEventListener("click", function () {
+    quantity("case-input-number", "casePrice", 59, true)
+})
+
+document.getElementById("case-minus-btn").addEventListener("click", function () {
+    quantity("case-input-number", "casePrice", 59, false)
 })
